f80d0d76e193a46a619f285b99789dd5308ed681
Jonte88 <jonathan.thj15@gmail.com>
2023-11-28 14:09:14 +0100

    Adding buttons to sidebar

--- Hjalp_website/src/SideBar.jsx
+++ Hjalp_website/src/SideBar.jsx
@@ -1,5 +1,6 @@
 // import { useState } from "react";
 import "./assets/css/sideBar.css";
+import { Link } from "react-router-dom";
 
 function SideBar() {
   //   const [count, setCount] = useState(0);
@@ -7,7 +8,15 @@ function SideBar() {
   return (
     <>
       <div className="SideBar">
-        
+        <ul className="sideBarNav">
+          <li>Elever</li>
+          <li>Klassrum</li>
+          <li>Schema</li>
+          <li>Othmans Sexcave</li>
+        </ul>
+        <Link to={"./"}>
+          <li>Registrera närvaro</li>
+        </Link>
       </div>
     </>
   );
