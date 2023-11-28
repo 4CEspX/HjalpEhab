6efb93c328fcaec18a76fc4e05335f261bea697e
Jonte88 <jonathan.thj15@gmail.com>
2023-11-28 10:09:46 +0100

    Chane som App stuff and adding my own code 	modified:   Hjalp_website/src/App.jsx 	modified:   Hjalp_website/src/assets/css/header.css

--- Hjalp_website/src/App.jsx
+++ Hjalp_website/src/App.jsx
@@ -1,31 +1,38 @@
 // Hjalp_website/src/App.js
 
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Header from "./header";
 
 function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     // Make a GET request to the server API
-    axios.get('http://localhost:3000/api/users')
-      .then(response => {
+    axios
+      .get("http://localhost:3000/api/users")
+      .then((response) => {
         setUsers(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   }, []);
 
   return (
-    <div>
-      <h1>User List</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>{user.username} - {user.email}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Header />
+      <div>
+        <h1>User List</h1>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name} - {user.klass} - {user.timestamp}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
