ad6c63b0748ae2ab33612cc5b70e69e8834ad2eb
Othmam-Al-Ani <142592326+Othmam-Al-Ani@users.noreply.github.com>
2023-11-28 13:52:05 +0100

    login in

--- my-app/app/LoginScreen.js
+++ my-app/app/LoginScreen.js
@@ -2,11 +2,15 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, Button} from 'react-native';
 import { Link } from 'expo-router';
-import Scanner from './Scanner';
+
 import { styles } from './index';
 
 
 
+const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const handleLogin = () => {
     // Validate the user's credentials
@@ -21,14 +25,27 @@ import { styles } from './index';
   return (
     <View style={styles.container}>
       <View style={styles.main}>
+        <Text style={styles.title}>Login</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          value={username}
+          onChangeText={setUsername}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
+        <Button title="Login" onPress={handleLogin} />
+        {error ? <Text style={styles.error}>{error}</Text> : null}
+        <Link  style={styles.link} href="Scanner">go to scanner</Link>
     
-      
-      <Text style={styles.title}>Under construction</Text>
-      <Link  style={styles.link} href="Scanner">go to scanner</Link>
-    
-    </View>
+      </View>
     </View>
   );
-}
+};
 
 export default LoginScreen;
