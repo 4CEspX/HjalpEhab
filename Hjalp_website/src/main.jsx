7e68826f1d34f3484b1823cce89e07d3098d0cfc
Jonte88 <jonathan.thj15@gmail.com>
2023-12-04 12:27:30 +0100

    Working some magiv with the links and navbar 	modified:   Hjalp_website/src/main.jsx

--- Hjalp_website/src/main.jsx
+++ Hjalp_website/src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import App from "./App.jsx";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import {
+  Link,
+  RouterProvider,
+  createBrowserRouter,
+  useParams,
+} from "react-router-dom";
+import "./App.css";
+import Header from "./Header";
+import SideBar from "./SideBar";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const Wrapper = () => (
+  <>
+    <Header />
+    <SideBar />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <Wrapper />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
