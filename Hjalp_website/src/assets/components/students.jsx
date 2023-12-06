aaf408f5961e0a9e8c92d196671cb269362b7918
Jonte88 <jonathan.thj15@gmail.com>
2023-12-06 09:26:56 +0100

    Removing unnecesary code 	modified:   Hjalp_website/src/assets/components/students.jsx

--- Hjalp_website/src/assets/components/students.jsx
+++ Hjalp_website/src/assets/components/students.jsx
@@ -1,25 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
+import React from "react";
 import "../css/students.css";
 
 function Students() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    console.log("Helloooooooooooo!");
-    // Make a GET request to the server API
-    axios
-      .get("http://192.168.220.50:3000/api/users")
-      .then((response) => {
-        setUsers(response.data);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
-
   return (
     <>
       <div className="studentsContainer">
